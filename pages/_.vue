026895eca2aceab79a11521e8985e4b08b62ec78
Stijn Vermeeren <stijn@stijnvermeeren.be>
2019-12-24 15:20:31 +0100

    add "page not found" page

---
+++ pages/_.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    h2 Pagina niet gevonden
+    div
+      | Ga naar de
+      |
+      nuxt-link(to="/") startpagina
+      | .
+</template>
+
+<script>
+
+  export default {
+    head: {
+      title: 'Pagina niet gevonden'
+    }
+  }
+</script>
