fc7203be487f110b815426b1d45a01c19c5bf6d9
Stijn Vermeeren <stijn@stijnvermeeren.be>
2020-12-28 20:16:24 +0100

    allow deleting comments

--- components/comments/Comment.vue
+++ components/comments/Comment.vue
@@ -1,21 +1,39 @@
 <template lang="pug">
   div(:class="['reactie', {'mine': isMine}]")
-    .reacinfo
-      span.name {{ comment.name }}
-      span.created {{ comment.created }}
-    .bericht {{ comment.message }}
+    template(v-if="!isDeleted")
+      .reacinfo
+        span.name {{ comment.name }}
+        span.created {{ comment.created }}
+        span.delete(v-if="isMine")
+          a(@click="deleteComment") Bericht verwijderen
+      .bericht {{ comment.message }}
+    div.deleted(v-else) Bericht verwijderd
 </template>
 
 <script>
   export default {
     name: 'Comment',
     props: ['comment'],
+    data() {
+      return {
+        isDeleted: false,
+      }
+    },
     computed: {
       isAuthenticated() {
         return this.$store.getters['auth/isAuthenticated'];
       },
       isMine() {
         return this.isAuthenticated && this.$store.state.auth.user.id === this.comment.userId;
+      }
+    },
+    methods: {
+      deleteComment() {
+        if (confirm("Wil je dit bericht werkelijk verwijderen?")) {
+          this.$axios.$delete(`comment/${this.comment.id}`).then(response => {
+            this.isDeleted = true;
+          });
+        }
       }
     }
   }
@@ -46,11 +64,21 @@
         margin-left: 1em;
         color: #888;
       }
+
+      span.delete {
+        margin-left: 1em;
+        color: #888;
+        font-size: 80%;
+      }
     }
 
     div.bericht {
       white-space: pre-wrap;
       overflow: auto;
     }
+  }
+
+  div.deleted {
+    font-style: italic;
   }
 </style>
--- pages/reacties/_page.vue
+++ pages/reacties/_page.vue
@@ -34,7 +34,6 @@
         const page = this.$route.params.page || 1;
         this.$axios.$get(`comments/${page}`).then(comments => {
           this.comments = comments;
-
         });
       },
       onSubmitted() {
