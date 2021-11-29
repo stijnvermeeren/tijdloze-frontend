fb73b4e9d33015e4fba73b2d982e87aa2adee797
Stijn Vermeeren <stijn@stijnvermeeren.be>
2021-11-29 18:35:20 +0100

    more flexibile search (ing - in, o - of)

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -135,6 +135,8 @@
           .toLowerCase()
           .normalize("NFD")
           .replace(/æ/g, "ae")
+          .replace(/ing\b/g, "in") // match e.g. "loving" with "lovin"
+          .replace(/\bo\b/g, "of") // match "o" with "of"
           .replace(/[\u0300-\u036f]/g, "") // combining accents
           .replace(/[^a-zA-Z0-9 ]/g, "")
       },
