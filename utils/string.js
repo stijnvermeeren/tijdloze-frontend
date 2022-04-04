fea2ad3fcf195e5cb65e974a56de5f5ed659a066
Stijn Vermeeren <stijn@stijnvermeeren.be>
2022-04-04 08:25:07 +0200

    flexibility for "2"="to" and "u"="you" in search

--- utils/string.js
+++ utils/string.js
@@ -6,6 +6,8 @@ export function normalize(input) {
     .replace(/æ/g, "ae")
     .replace(/ing\b/g, "in") // match e.g. "loving" with "lovin"
     .replace(/\bo\b/g, "of") // match "o" with "of"
+    .replace(/\b2\b/g, "to") // match "2" with "to"
+    .replace(/\bu\b/g, "you") // match "u" with "you"
     .replace(/[\u0300-\u036f]/g, "") // combining accents
     .replace(/[^a-zA-Z0-9 ]/g, "")
 }
