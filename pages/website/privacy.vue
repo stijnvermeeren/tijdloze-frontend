e4b296672ba3288c041c400cbc8d2e16fe522f6a
Stijn Vermeeren <stijn@stijnvermeeren.be>
2022-06-08 21:11:39 +0200

    improved privacy policy

--- pages/website/privacy.vue
+++ pages/website/privacy.vue
@@ -3,8 +3,11 @@
     h2 Privacybeleid
 
     h3 Registreren en aanmelden
-    p Voor enkele functies van de Tijdloze Website (zoals de kommentaarfunctie en de chatbox) is een aanmelding noodzakelijk. Iedere registratie en login verloopt via de dienst #[a(href='https://auth0.com/') Auth0]. Voor gebruikers die met gebruikersnaam en wachtwoord registreren, wordt het wachtwoord veilig bewaard door Auth0 en niet met de Tijdloze Website gedeeld. Aan gebruikers die zich via andere diensten (bijvoorbeeld Facebook of Google) aanmelden, wordt toestemming gevraagd om enkel de meeste essentiële gegevens (gebruikersnaam, naam en emailadres) te delen met Auth0 en met de Tijdloze Website. Iedere gebruiker heeft de mogelijkheid een aparte gebruikersnaam die kiezen voor de Tijdloze Website. Deze gekozen gebruikersnaam is de enige persoonlijke informatie die zichtbaar is voor andere bezoekers van de Tijdloze Website. Bij alle aangemelde bezoekers worden er cookies geplaatst om ervoor te zorgen dat het aanmelden niet steeds nodig is bij elk volgend bezoek aan de Tijdloze Website.
-
+    p Voor enkele functies van de Tijdloze Website (zoals de kommentaarfunctie en de chatbox) is een aanmelding noodzakelijk. Iedere registratie en login verloopt via de dienst #[a(href='https://auth0.com/') Auth0]. Voor gebruikers die met gebruikersnaam en wachtwoord registreren, wordt het wachtwoord veilig bewaard door Auth0 en niet met de Tijdloze Website gedeeld. Aan gebruikers die zich via andere diensten (bijvoorbeeld Facebook of Google) aanmelden, wordt toestemming gevraagd om enkel de meeste essentiële gegevens te delen met Auth0 en met de Tijdloze Website, namelijk:
+    ul
+      li Naam / gebruikersnaam: deze wordt overgenomen als gebruikersnaam voor de Tijdloze Website, tenzij de bezoeker zelf een andere gebruikersnaam voor de Tijdloze Website kiest. De gekozen gebruikersnaam is de enige persoonlijke informatie die zichtbaar is voor andere bezoekers van de Tijdloze Website.
+      li E-mailadres: wordt gebruikt om te kunnen antwoorden op vragen van de gebruiker en om indien nodig de identiteit van de gebruiker te controleren. Het e-mailadres van een gebruiker wordt nooit getoond aan andere bezoekers.
+    p Bij alle aangemelde bezoekers worden er cookies geplaatst om ervoor te zorgen dat het aanmelden niet steeds nodig is bij elk volgend bezoek aan de Tijdloze Website.
     p Indien een aangemelde gebruiker wenst om de eigen gebruikersaccount volledig te verwijderen en alle persoonlijke gegevens te laten wissen uit de databank van de Tijdloze Website, dan kan dit door een aanvraag te sturen via het #[nuxt-link(to='/website/contact') contactformulier].
 
     h3 Analytics
