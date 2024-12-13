0ee18f743863310abf5bdc1500b2b5e4d15c3f25
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2024-12-13 11:58:28 +0100

    trigger musicbrainz crawl for albums

--- pages/admin/crawl-album.vue
+++ pages/admin/crawl-album.vue
@@ -7,8 +7,18 @@ div
   h3 Volgende voorstel van crawler
 
   admin-crawl-process(type="album")
+
+  hr
+
+  v-btn.mt-5(@click="crawl") crawl by song
 </template>
 
 <script setup>
 definePageMeta({ middleware: 'admin' })
+
+const {$api} = useNuxtApp()
+
+async function crawl() {
+  await $api('musicbrainz/crawl-songs')
+}
 </script>
