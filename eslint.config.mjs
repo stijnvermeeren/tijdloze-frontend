6a5ce6ea601c7efdb679f46cbe628cec528d7ae6
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2025-08-31 15:52:24 +0200

    fix eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
-import pluginVue from 'eslint-plugin-vue'
 import pluginVuePug from 'eslint-plugin-vue-pug'
 
-export default withNuxt([
-  ...pluginVue.configs['flat/recommended'],
-  ...pluginVuePug.configs['flat/recommended'],
-])
+export default withNuxt(
+  ...pluginVuePug.configs['flat/recommended']
+)
