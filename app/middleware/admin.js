cfbb26f01890bc2898b1b9864ed3805d473c42aa
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2025-09-23 20:50:54 +0200

    fix admin redirect

--- app/middleware/admin.js
+++ app/middleware/admin.js
@@ -3,15 +3,19 @@ import {authGuard, useAuth0} from "@auth0/auth0-vue";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const authStore = useAuthStore()
-  const auth0 = useAuth0()
-
-  const loggedIn = await authGuard(to)
-  if (loggedIn) {
-    await useSetUser(auth0)
-    if (!authStore.isAdmin) {
+  
+  if (import.meta.client) {
+    const auth0 = useAuth0()
+    const loggedIn =  await authGuard(to)
+    if (loggedIn) {
+      await useSetUser(auth0)
+      if (!authStore.isAdmin) {
+        return navigateTo('/', {replace: true, external: true})
+      }
+    } else {
       return navigateTo('/', {replace: true, external: true})
     }
   } else {
-    return navigateTo('/', {replace: true, external: true})
+    return navigateTo({path: '/auth/login', query: {redirect: to.fullPath}}, {replace: true})
   }
 })
