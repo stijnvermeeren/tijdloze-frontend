526602afba2f189f04518db03ba7208d87e9bf38
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2026-08-18 22:22:48 +0200

    env config change for AWS amplify

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,16 +6,7 @@ module.exports = {
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs',
-            env: {
-                "NUXT_PUBLIC_API_URI_CLIENT": "",
-                "NUXT_PUBLIC_API_URI_SERVER": "",
-                "NUXT_PUBLIC_AUTH0_CLIENT_ID": "",
-                "NUXT_PUBLIC_AUTH0_CLIENT_DOMAIN": "",
-                "NUXT_PUBLIC_AUTH0_AUDIENCE": "",
-                "NUXT_PUBLIC_AUTH0_CALLBACK_URI": "",
-                "NUXT_PUBLIC_AUTH0_LOGOUT_URI": "",
-                "NUXT_PUBLIC_GTAG_ID": ""
-            }
+            env_file: '.env',
         }
     ]
 }
